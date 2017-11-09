e5bcfad59dab90117361fc677a392fe6ddc3d4ca
andyliu <673988904@qq.com>
2017-11-09 18:36:20 +0800

    引入bootstrap

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// ap.set('view engine','html');
+// app.engine('html',require('ejs').__express);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', index);
 app.use('/users', users);
 
